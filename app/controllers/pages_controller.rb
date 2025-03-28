bea91a0291a970a00775bb9217a8eb72c055fac5
Julian <jdpineda@miuandes.cl>
2025-03-28 17:44:49 -0300

    lab 3 hecho

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+    if request.post?
+      flash[:notice] = "Thank you for your message, we will contact you soon."
+      redirect_to contact_path
+    end
+  end
+end
