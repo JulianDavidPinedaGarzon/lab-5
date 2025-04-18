0a2756b473d506cd2e6987d033eac4a42277f6c4
JulianDavidPinedaGarzon <jdpineda@miuandes.cl>
2025-04-18 15:26:45 -0400

    Lab 06: Added views and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get "messages/index"
+  get "messages/show"
+  get "chats/index"
+  get "chats/show"
+  get "users/index"
+  get "users/show"
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   post 'contact', to: 'pages#contact'
+  resources :users, only: [:index, :show]
+  resources :chats, only: [:index, :show]
+  resources :messages, only: [:index, :show]
+  root "users#index"
 end
