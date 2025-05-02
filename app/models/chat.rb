7c05ba23eef6fb15632b2115515e4184340e6a54
JulianDavidPinedaGarzon <jdpineda@miuandes.cl>
2025-05-02 17:31:09 -0400

    lab7

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,2 +1,16 @@
 class Chat < ApplicationRecord
-end
+    belongs_to :sender, class_name: "User"
+    belongs_to :receiver, class_name: "User"
+  
+    has_many :messages, dependent: :destroy
+  
+    validates :sender_id, :receiver_id, presence: true
+    validate :different_users
+  
+    private
+  
+    def different_users
+      errors.add(:receiver_id, "must be different from sender") if sender_id == receiver_id
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
-  get "messages/index"
-  get "messages/show"
-  get "chats/index"
-  get "chats/show"
-  get "users/index"
-  get "users/show"
-  root 'pages#home'
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
-  post 'contact', to: 'pages#contact'
-  resources :users, only: [:index, :show]
-  resources :chats, only: [:index, :show]
-  resources :messages, only: [:index, :show]
+  resources :users
+  resources :chats
+  resources :messages
+
   root "users#index"
 end
